e2bb4151aca6b3c1dc0bcde4b2b0037be6fea888
n38803 <shaunthompson@fullsail.edu>
2014-12-18 20:41:33 -0500

    add & save actionbar complete w/save feature done

--- JavaII-W4/app/src/main/java/android/fullsail/com/javaii_w4/AddActivity.java
+++ JavaII-W4/app/src/main/java/android/fullsail/com/javaii_w4/AddActivity.java
@@ -1,7 +1,158 @@
 package android.fullsail.com.javaii_w4;
 
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.fullsail.com.javaii_w4.dataclass.Item;
+import android.fullsail.com.javaii_w4.fragment.AddFragment;
+import android.os.Bundle;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
 /**
  * Created by Shaun on 12/18/2014.
  */
-public class AddActivity {
+public class AddActivity extends Activity {
+
+    public TextView inputItem;
+
+    public String mItem;
+    private ActionMode mActionMode;
+
+    private ArrayList<Item> mItemList;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+
+        // Check to see whether or not there is a saved instance of the fragment
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new AddFragment())
+                    .commit();
+        }
+
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        // Grabs intent from main activity
+        Intent addIntent = getIntent();
+
+        // verify there is an intent available to grab
+        if (addIntent != null) {
+
+            // do stuff after grabbing intent.
+        }
+
+
+    }
+
+    public void onCancel(View v) {
+        clearDisplay();
+        finish();
+
+
+    }
+
+    public void onSave(View v) {
+        inputItem = (TextView) findViewById(R.id.inputItem);
+
+
+        // assign input to variables
+        mItem = inputItem.getText().toString();
+
+
+        // save variables to contact object
+        //mContactDataList = new ArrayList<Contact>();
+        // mContactDataList.add(new Contact(cName, cEmail, cLocation));
+
+        Intent intent = new Intent();
+        intent.putExtra("item", mItem);
+        intent.putExtra("action", "add");
+        setResult(RESULT_OK, intent);
+
+        clearDisplay();
+        finish();
+
+
+    }
+
+    private void clearDisplay() {
+
+        inputItem = (TextView) findViewById(R.id.inputItem);
+        inputItem.setText("");
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.actionbar_save, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        AlertDialog.Builder save = new AlertDialog.Builder(this);
+        save.setTitle("Title of alert dialog");
+        save.setIcon(android.R.drawable.ic_dialog_alert);
+
+        // USER ELECTS TO SAVE INFORMATION
+        save.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+
+                // assign view & input to constant
+                inputItem = (TextView) findViewById(R.id.inputItem);
+                mItem = inputItem.getText().toString();
+
+                // pass information to intent
+                Intent intent = new Intent();
+                intent.putExtra("item", mItem);
+                intent.putExtra("action", "add");
+                setResult(RESULT_OK, intent);
+
+
+                // TODO - CLEAR DISPLAY METHOD
+
+                // close current activity
+                finish();
+
+
+
+            } });
+
+        // USER ELECTS TO DISCARD INFORMATION
+        save.setNegativeButton("Discard", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+
+                // TODO - CLEAR DISPLAY METHOD
+
+                finish();
+
+
+            } });
+        save.show();
+
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
+
+
--- JavaII-W4/app/src/main/java/android/fullsail/com/javaii_w4/MainActivity.java
+++ JavaII-W4/app/src/main/java/android/fullsail/com/javaii_w4/MainActivity.java
@@ -1,14 +1,20 @@
 package android.fullsail.com.javaii_w4;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.app.DialogFragment;
 import android.content.Intent;
 import android.fullsail.com.javaii_w4.dataclass.Item;
 import android.fullsail.com.javaii_w4.fragment.MainListFragment;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ActionMode;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -16,14 +22,20 @@ import java.util.ArrayList;
 public class MainActivity extends Activity implements MainListFragment.ContactListener {
 
     private final String TAG = "MainActivity";
+    public final int ADDREQUEST = 1;
 
     private ArrayList<Item> mItemList;
     public Button addButton;
+    private ActionMode mActionMode;
+    private int mItemSelected = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
         // Check to see whether or not there is a saved instance of the fragment
         if (savedInstanceState == null) {
@@ -31,6 +43,8 @@ public class MainActivity extends Activity implements MainListFragment.ContactLi
                     .add(R.id.container, new MainListFragment())
                     .commit();
         }
+
+
 
 
         // static population of data
@@ -49,7 +63,7 @@ public class MainActivity extends Activity implements MainListFragment.ContactLi
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.actionbar_add, menu);
         return true;
     }
 
@@ -58,12 +72,82 @@ public class MainActivity extends Activity implements MainListFragment.ContactLi
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+
+        Intent addIntent = new Intent(MainActivity.this, AddActivity.class);
+        startActivityForResult(addIntent, ADDREQUEST);
+        //startActivity(addIntent);
+
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
+
+    // INTENT HANDLER
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode == ADDREQUEST && resultCode == RESULT_OK){
+            String requestInfo = data.getStringExtra("item");
+            String action = data.getStringExtra("action");
+
+            // add item to list
+            mItemList.add(new Item(requestInfo));
+
+            // update display
+            MainListFragment nf = (MainListFragment) getFragmentManager().findFragmentById(R.id.container);
+            nf.updateListData();
+
+
+            if (action.equals("add")){
+                Toast.makeText(this, requestInfo + " added to List.", Toast.LENGTH_LONG).show();
+
+
+            }
+        }
+    }
+
+
+    // CONTECTUAL ACTION BAR
+    private ActionMode.Callback mActionModeCallback = new ActionMode.Callback() {
+
+        // Called when the action mode is created; startActionMode() was called
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            // Inflate a menu resource providing context menu items
+            MenuInflater inflater = mode.getMenuInflater();
+            inflater.inflate(R.menu.actionbar_add, menu);
+            return true;
+        }
+
+        // Called each time the action mode is shown. Always called after onCreateActionMode, but
+        // may be called multiple times if the mode is invalidated.
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false; // Return false if nothing is done
+        }
+
+        // Called when the user selects a contextual menu item
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+
+            // TODO - SET WHAT HAPPENS WHEN USER CLICKS ITEM
+
+            return false;
+
+        }
+
+        // Called when the user exits the action mode
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+            mActionMode = null;
+        }
+    };
+
+
+
+
+
+    // INTERFACE METHODS
 
     public void viewContact(int position){
 
